2d7e7edae5dc73c68b9d82cc6c7bd7ca54232fdc
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-04-30 22:27:50 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import react from "react"
+import List from "./components/List"
+import Container from "./Container"
+
+function App() {
+  return (
+    <div>
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from 'react';
+import ListItem from './ListItem';
+
+class List extends Component {    
+    render(){
+        const showList = this.props.data.data.map(item => <ListItem key={item.id} item={item} clickItem={this.props.data.clickItem}/>)
+        return (
+            <ul>
+                {showList}
+            </ul>
+        )
+    }
+}
+
+export default List;
